63883bc6fd9ae68e62eb5742d03d8ff6fda5a353
SAWAIZ2003 <mk4914@srmist.edu.in>
2025-01-17 22:35:28 +0530

    first rust cod

---
+++ main.rs
@@ -0,0 +1,34 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+
+fn main() {
+    let ans = is_even(-2184999948290380);
+    println!("{}" , ans);
+}
+
+fn is_even(num: i64) -> bool {
+     if num % 2 == 0 {
+        return true;
+     }
+     else  {
+        return false;
+     }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
